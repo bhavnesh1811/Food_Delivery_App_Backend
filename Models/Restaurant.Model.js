efd0e23fefdc0e77234f25baa393b62b160a3c4c
bhavnesh1811 <erbhavnesharora@gmail.com>
2023-06-10 21:53:46 +0530

    added order and restaurant model

--- Models/Restaurant.Model.js
+++ Models/Restaurant.Model.js
@@ -1,23 +1,23 @@
 const mongoose = require("mongoose");
 
-const restaurantSchema = mongoose.Schema({
-  name: String,
-  address: {
-    street: String,
-    city: String,
-    state: String,
-    country: String,
-    zip: String,
-  },
-  menu: [
-    {
-      name: String,
-      description: String,
-      price: Number,
-      image: String,
-    },
-  ],
-});
+// const restaurantSchema = mongoose.Schema({
+//   name: String,
+//   address: {
+//     street: String,
+//     city: String,
+//     state: String,
+//     country: String,
+//     zip: String,
+//   },
+//   menu: [
+//     {
+//       name: String,
+//       description: String,
+//       price: Number,
+//       image: String,
+//     },
+//   ],
+// });
 
 const RestaurantModel = mongoose.model("restaurant", restaurantSchema);
 
